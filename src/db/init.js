75a4d87f0892ad138f0dd8cfb722653d934c8f0e
Unwana Udo <unwana.udo@flo-dojo.com>
2024-10-19 18:56:50 -0600

    chore: add CRUD service functions and tests for blog post management

---
+++ src/db/init.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+export function initDatabase() {
+  const DATABASE_URL =
+    process.env.DATABASE_URL || 'mongodb://localhost:27017/reviews'
+  mongoose.connect(DATABASE_URL)
+
+  mongoose.connection.on('connected', () => {
+    console.info('Connected to the MongoDB database successfully')
+  })
+}
